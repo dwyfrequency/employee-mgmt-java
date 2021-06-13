08d715ff85619a333f5ed431916c6323353ce097
dwyfrequency <jdwy215@gmail.com>
2021-06-13 11:49:16 -0400

    add controller api methods

---
+++ src/main/java/com/jackiedecodes/employeemanager/EmployeeResource.java
@@ -0,0 +1,45 @@
+package com.jackiedecodes.employeemanager;
+
+import com.jackiedecodes.employeemanager.model.Employee;
+import com.jackiedecodes.employeemanager.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+    private final EmployeeService employeeService;
+
+    public EmployeeResource(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return new ResponseEntity<>(employeeService.findAllEmployees(), HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        return new ResponseEntity<>(employeeService.findEmployeeById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.addEmployee(employee), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.updateEmployee(employee), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") Long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
